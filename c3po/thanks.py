a34b457b57cf4fea087f76bb54ab2cd66a2061fd
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2021-04-06 11:49:21 -0500

    Add instructions to each page.

--- c3po/thanks.py
+++ c3po/thanks.py
@@ -13,7 +13,7 @@ bp = Blueprint('thanks', __name__, url_prefix='/thanks')
 @bp.route('/<thanks_type>', methods=('GET', 'POST'))
 def thanks(thanks_type):
 
-    text = 'Thank you for registering to participate, all applicable authors should receive emails with links shortly!'
+    text = 'Thank you for registering to participate, all applicable authors should receive emails with links shortly! Each email address will receive individual emails for each article they were registered for. Simply click on the link in the email to enter information to the relevant article.'
     if thanks_type == "submission":
         text = 'Thank you for submitting your publication information!'
 
