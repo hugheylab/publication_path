ccb0c1055673ec9ca4f0444c4abbfbed8ba97ad4
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2021-05-21 11:17:07 -0500

    Fix wrong option call

--- c3po/get_pmc_emails.sh
+++ c3po/get_pmc_emails.sh
@@ -1,8 +1,8 @@
 cd pmc_output
 wget https://www.dropbox.com/s/53febkxfinckfkk/pmc_email.csv?dl=1
 mv 'pmc_email.csv?dl=1' pmc_email.csv
-psql pmdb -q "DELETE FROM pmc_email;"
-psql pmdb -q "COPY pmc_email(doi, email, pmc) 
+psql pmdb -c "DELETE FROM pmc_email;"
+psql pmdb -c "COPY pmc_email(doi, email, pmc) 
 FROM '/home/ubuntu/pmc_output/pmc_email.csv' 
 DELIMITER ','
 CSV HEADER;"
